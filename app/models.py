2e6ef339ce60fe9d6cb78e387fa28c6728113e18
Nikita <nikita19961032@gmail.com>
2024-02-23 20:34:19 +0300

    migrations

--- app/models.py
+++ app/models.py
@@ -14,12 +14,11 @@ class Country(models.Model):
 
 class Account(models.Model):
     country = models.ForeignKey(Country, on_delete=models.PROTECT, verbose_name="Страна")
-    post = models.CharField(blank=True, null=True, max_length=50, verbose_name="Почта")
-    password = models.CharField(blank=True, null=True, max_length=20, verbose_name="Пароль")
-    config_number = models.IntegerField(blank=True, null=True, default=0, verbose_name="Номер конфига")
-    ip = models.ManyToManyField('Ip', blank=True, null=True, verbose_name="IP адрес")
-    creator = models.ForeignKey('Creator', blank=True, null=True, on_delete=models.PROTECT, verbose_name="Создатель")
-    data = models.DateTimeField(blank=True, null=True, verbose_name="Дата создания")
+    post = models.CharField(max_length=50, verbose_name="Почта")
+    password = models.CharField(max_length=20, verbose_name="Пароль")
+    config_number = models.IntegerField(default=0, verbose_name="Номер конфига")
+    creator = models.ForeignKey('Creator', on_delete=models.PROTECT, verbose_name="Создатель")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     class Meta:
         verbose_name = "Аккаунт"
@@ -31,6 +30,7 @@ class Account(models.Model):
 
 class Ip(models.Model):
     ip = models.GenericIPAddressField(verbose_name="IP адрес")
+    account = models.ForeignKey(Account, on_delete=models.PROTECT, blank=True, null=True, verbose_name="Аккаунт")
     country = models.ForeignKey(Country, on_delete=models.PROTECT, verbose_name="Страна")
 
     class Meta:
